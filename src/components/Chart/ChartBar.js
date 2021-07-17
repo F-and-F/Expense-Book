af85ffc0a39fc33ee0fccd557d5ecebce792fe1e
F-and-F <farzin.azimzade@protonmail.com>
2021-07-17 20:56:44 +0430

    commy

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./ChartBar.css";
 const ChartBar = (props) => {
+  //comment
   return (
     <div className="chart-bar">
       <div className="chart-bar__inner">
